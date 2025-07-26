ccc002f0a324d256c0c7f29ee8eba039c9e7fd69
Caner Yanbaz <103248957+cyanbaz@users.noreply.github.com>
2025-07-26 18:29:15 +0200

    feat(comment): Comments added (#24)

--- src/main/java/com/github/cyanbaz/jenkins/plugins/jsonparameter/JsonResult.java
+++ src/main/java/com/github/cyanbaz/jenkins/plugins/jsonparameter/JsonResult.java
@@ -1,5 +1,16 @@
+/**
+ * Copyright (c) 2025 Caner Yanbaz
+ * Licensed under the MIT License (see LICENSE file).
+ */
 package com.github.cyanbaz.jenkins.plugins.jsonparameter;
 
+/**
+ * A generic result container used to wrap the outcome of loading JSON data.
+ * Includes a success flag, the result value (if successful), and an optional error message.
+ *
+ * @param <T> The type of the value.
+ * @author Caner Yanbaz
+ */
 public class JsonResult<T> {
 
     private final boolean success;
